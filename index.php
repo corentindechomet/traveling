aec5add8f5a69a05bf4118683524e1bb2aa78401
henrirespaudbouny <henrirespaudbouny@gmail.com>
2016-10-11 20:37:35 +0200

    Organisation dossier + normalize.css (fichier css qui reset les modifs des différents browsers)

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>Traveling</title>
+  <link rel="stylesheet" href="normalize_v5.css">
   <link rel="stylesheet" href="style.css">
   <script src="script.js"></script>
 </head>
