6545a848fb15cf4e86bc7b1eb9ad1de8763f0fab
corentindechomet <corentindechomet@gmail.com>
2016-10-11 21:56:49 +0200

    index dev

--- index.php
+++ index.php
@@ -1,17 +1,47 @@
 <!doctype html>
 <html lang="fr">
 <head>
-  <meta charset="utf-8">
-  <title>Traveling</title>
-  <link rel="stylesheet" href="css/normalize_v5.css">
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
+	<meta charset="utf-8">
+	<title>Traveling</title>
+	<link rel="stylesheet" href="css/normalize_v5.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-  ...
-  <!-- Le reste du contenu -->
-  ...
+	<!-- Navbar -->
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#home">TRAVELING</a>
+			</div>
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li id="anchor1"><a href="#services">Search by film</a></li>
+					<li id="anchor2"><a href="#projets">Projets</a></li>
+					<li id="anchor3"><a href="#contact">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<!-- FIN Navbar -->
+
+    <header id="home">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <img src="imgs/logoTraveling.png" alt="logoTraveling" width="100%">
+                </div>
+            </div>
+        </div>        
+    </header>
+	...
 </body>
-  <script src="js/jquery-3.1.1.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </html>
