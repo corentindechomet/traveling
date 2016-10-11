68bc0508433fa053fcdafc8826c87d023b391250
corentindechomet <corentindechomet@gmail.com>
2016-10-11 21:22:21 +0200

    add assets

--- index.php
+++ index.php
@@ -3,13 +3,15 @@
 <head>
   <meta charset="utf-8">
   <title>Traveling</title>
-  <link rel="stylesheet" href="normalize_v5.css">
-  <link rel="stylesheet" href="style.css">
-  <script src="script.js"></script>
+  <link rel="stylesheet" href="css/normalize_v5.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
   ...
   <!-- Le reste du contenu -->
   ...
 </body>
+  <script src="js/jquery-3.1.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
 </html>
