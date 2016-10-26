b743e1b799cedabf5a3d0fab768388f6281568c4
corentindechomet <corentindechomet@gmail.com>
2016-10-26 22:25:11 +0200

    index.php changes, add fonts, images & change script

--- index.php
+++ index.php
@@ -5,9 +5,25 @@
 	<title>Traveling</title>
 	<link rel="stylesheet" href="css/normalize_v5.css">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+	<div id="myNavTop" class="topPart">
+		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">X</a>
+		<div class="topPart-content">
+
+		</div>
+	</div>
+	<div id="myNavBottom" class="bottomPart">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="bottomPart-content"></div>
+				</div>
+			</div>
+		</div>
+	</div>
 	<!-- Navbar -->
 	<nav class="navbar navbar-default navbar-fixed-top">
 		<div class="container">
@@ -51,41 +67,50 @@
 						$resultatP = $connection->query($requeteP);
 						$tabP = $resultatP->fetchAll(PDO::FETCH_OBJ);
 					?>
-					<div class="lastFilmAdded card" style="background: url('<?php echo $tabP[0]->urlimg; ?>')">
+					<div class="card overlay lastFilmAdded" style="background: url('<?php echo $tabP[0]->urlimg; ?>') top;">
 						<h2><span>LAST FILM ADDED</span></br><?php echo $tabP[0]->titreOeuvre; ?><h2>			
 					</div>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-6 col-xs-12">
-					<div id="searchByFilms" class="searchBy card">
+					<div id="searchByFilms" class="card searchBy">
 						<input type="search" placeholder="Search by series or movies" name="the_search">
+						<i class="searchIcon fa fa-search" aria-hidden="true"></i>
 					</div>
 				</div>
 				<div class="col-md-6 col-xs-12">
-					<div id="searchByLocation" class="searchBy card">Search by series</div>
+					<div id="searchByLocation" class="card searchBy">
+						<input type="search" placeholder="Search by location" name="the_search">
+						<i class="searchIcon fa fa-search" aria-hidden="true"></i>
+					</div>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-4">
-					<div class="lastFilmAdded card">
-						<h2>DESIRED FOR URBAN</h2>
+					<div class="card urban">
+						<h3>DESIRED FOR <h3 class="transparent"><span class="urbanText">URBAN</span></h3></h3>
 					</div>
 				</div>
 				<div class="col-md-4">
-					<div class="lastFilmAdded card">
-						<h2>DESIRED FOR NATURE</h2>
+					<div class="card nature">
+						<h3>DESIRED FOR <h3 class="transparent"><span class="natureText">NATURE</span></h3></h3>
 					</div>
 				</div>
 				<div class="col-md-4">
-					<div class="lastFilmAdded card">
-						<h2>DESIRED FOR ESCAPE</h2>
+					<div class="card escape">
+						<h3>DESIRED FOR <h3 class="transparent"><span class="escapeText">ESCAPE</span></h3></h3>
 					</div>
 				</div>
 			</div>		
 			<div class="row">
 				<div class="col-md-12">
-					<div class="lastFilmAdded card">
+					<?php
+						$requeteP = 'SELECT * FROM oeuvres ORDER BY RAND()';
+						$resultatP = $connection->query($requeteP);
+						$tabP = $resultatP->fetchAll(PDO::FETCH_OBJ);
+					?>
+					<div class="card overlay randomFilm" target-url="<?php echo $tabP[0]->idOeuvre; ?>">
 						<h2>RANDOM FILM OR SHOW</h2>
 					</div>
 				</div>
@@ -95,4 +120,6 @@
 </body>
 <script src="js/jquery-3.1.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
+
 </html>
