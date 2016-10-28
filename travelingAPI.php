72afcf062e4a10981b7bc79da51ae614e1751634
corentindechomet <corentindechomet@gmail.com>
2016-10-28 22:13:30 +0200

    API

---
+++ travelingAPI.php
@@ -0,0 +1,23 @@
+<?php
+include('connection.php');
+$call=$_GET['call'];
+switch ($call) {
+	case 'oeuvre':
+
+	$id=$_GET['id'];
+	$requeteP = 'SELECT * FROM oeuvres, scene WHERE oeuvres.idOeuvre ='.$id.' AND scene.idOeuvre ='.$id ;
+
+	$resultatP = $connection->query($requeteP);
+	$tabP = $resultatP->fetchAll(PDO::FETCH_OBJ);
+
+	echo json_encode($tabP);
+
+	break;
+
+	case 'markers':
+
+	default:
+		# code...
+	break;
+}
+?>
