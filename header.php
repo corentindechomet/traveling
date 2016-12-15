1d5dbbe3ab7e80cd2dad45d04dcd84703cdec3b2
corentindechomet <corentindechomet@gmail.com>
2016-12-15 11:15:15 +0100

    style

--- header.php
+++ header.php
@@ -1,9 +1,8 @@
 <head>
 	<meta charset="utf-8">
 	<title>Traveling</title>
-<!-- 	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-	 -->	
-	 <meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	<meta name="viewport" content="width=device-width">
 	<link rel="stylesheet" href="css/normalize_v5.css">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/font-awesome.min.css">
