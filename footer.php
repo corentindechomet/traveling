e76e008b3e7405e70dce4e8b3d36fafb45d684b8
corentindechomet <corentindechomet@gmail.com>
2016-12-16 18:00:08 +0100

    footer

--- footer.php
+++ footer.php
@@ -2,10 +2,12 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-4 col-sm-4 col-xs-4">
-						<img src="imgs/logoWhite.png" alt="" style="width:100px">
+						<img src="imgs/logoWhite.png" alt="" style="width:125px">
 					</div>
 					<div class="col-md-4 col-md-offset-4 col-sm-5 col-sm-offset-3 col-xs-6 col-xs-offset-2">
-						<p>MADE BY STUDENTS WITH CREATIVE IDEAS</p>
+						<a class='contribButton' href="./contact.php" target="_blank">
+							<h3>UNE CONTRIBUTION ?AIDEZ NOUS EN CLIQUANT ICI !</h3>
+						</a>
 					</div>
 				</div>
 			</div>
